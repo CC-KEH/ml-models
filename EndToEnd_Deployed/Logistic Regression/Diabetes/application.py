7a6c7d2da6e4c3eee28202260932f53fe8453233
CC-KEH <arbashhussain08@gmail.com>
2023-07-24 00:05:36 +0530

    deployment setup for diabetes

---
+++ EndToEnd_Deployed/Logistic Regression/Diabetes/application.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, app, render_template
+from flask import Response
+import pickle
+import numpy as np
+import pandas as pd
+
+application = Flask(__name__)
+app = application
+scaler = pickle.load(open('Model/standardScaler.pkl','rb'))
+model = pickle.load(open('Model/modelForPrediction.pkl','rb'))
+
+## Route for homepage
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predictdata',methods=['GET','POST'])
+def predict_datapoint():
+    result = ''
+    if request.method == 'POST':
+        Pregnancies = int(request.form.get('Pregnancies'))
+        Glucose = float(request.form.get('Glucose'))
+        BloodPressure = float(request.form.get('BloodPressure'))
+        SkinThickness = float(request.form.get('SkinThickness'))
+        Insulin = float(request.form.get('Insulin'))
+        DiabetesPedigreeFunction = float(request.form.get('DiabetesPedigreeFunction'))
+        Age = float(request.form.get('Age'))
